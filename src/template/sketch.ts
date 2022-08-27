f04a12c9ecb79632e0a75e958a6e68c91948de13
ocello3 <tkaa.2160@gmail.com>
2022-08-27 20:23:37 +0900

    template done

--- src/template/sketch.ts
+++ src/template/sketch.ts
@@ -1,8 +1,8 @@
+import p5 from "p5";
 import { controller } from "../util/controller";
 import { drawFrame } from "../util/drawFrame";
 import { tools } from "../util/tools";
 import { lib } from "./lib";
-import p5 from "p5";
 import { synth } from "./synth.js";
 
 export const sketch = (s: p5) => {
@@ -18,16 +18,14 @@ export const sketch = (s: p5) => {
     lib.setGui(libParams, tab);
     synth.setGui(synthParams, tab);
     s.noLoop();
-    s.frameRate(10);
+    // s.frameRate(10);
   };
   s.draw = () => {
     s.background(255);
     controller.updateController(s, controllers);
     libData = lib.updateData(libData, libParams, size);
     lib.draw(libData, libParams, s);
-    // debug(attractorObj);
     drawFrame(s, size);
-    controller.updateController(s, controllers);
     synth.playSynth(libData, synthData, synthParams, size);
   };
 };
